099ce442da4230bd33b35e3278d8b5b37282a476
RezaHabrizal <jreza.habrizal@gmail.com>
2021-06-30 06:20:29 +0700

    add router tailwind init edit page

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,47 +1,71 @@
 <template>
-  <div class="hello">
-    <div v-for="people in karyawan" :key="people.id">
-      {{ people.name }}
-    </div>
-  </div>
+  <tr>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <div class="flex items-center">
+        <div class="flex-shrink-0 h-10 w-10">
+          <img
+            class="h-10 w-10 rounded-full"
+            src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+            alt=""
+          />
+        </div>
+        <div class="ml-4">
+          <div class="text-sm font-medium text-gray-900">
+            {{ person.name }}
+          </div>
+          <div class="text-sm text-gray-500">
+            {{ person.email }}
+          </div>
+        </div>
+      </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <div class="text-sm text-gray-900">{{ person.title }}</div>
+      <div class="text-sm text-gray-500">
+        {{ person.divisi }}
+      </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <span
+        class="
+          px-2
+          inline-flex
+          text-xs
+          leading-5
+          font-semibold
+          rounded-full
+          bg-green-100
+          text-green-800
+        "
+      >
+        {{ person.status ? person.status : "null" }}
+      </span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+      <h6
+        @click="goEdit"
+        class="text-indigo-600 hover:text-indigo-900 cursor-pointer"
+      >
+        Edit
+      </h6>
+    </td>
+  </tr>
 </template>
 
 <script>
-import gql from "graphql-tag";
-
 export default {
   name: "HelloWorld",
   props: {
-    msg: String,
+    person: Object,
   },
-  apollo: {
-    karyawan: gql`
-      query getKaryawan {
-        karyawan {
-          _id
-          name
-          email
-        }
-      }
-    `,
+  methods: {
+    goEdit() {
+      this.$router.push("/edit");
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
---
+++ client/src/views/Edit.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">
+            Personal Information
+          </h3>
+          <p class="mt-1 text-sm text-gray-600">
+            Please complete your personal information.
+          </p>
+        </div>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form @submit.prevent="edit">
+          <div class="shadow sm:rounded-md">
+            <div class="px-4 py-5 bg-gray-100 sm:p-6">
+              <div class="grid grid-cols-8 gap-6">
+                <div class="col-span-6 sm:col-span-4">
+                  <label
+                    for="first_name"
+                    class="block text-sm font-medium text-gray-700"
+                    >Full name</label
+                  >
+                  <input
+                    type="text"
+                    class="
+                      mt-1
+                      focus:ring-indigo-500
+                      focus:border-indigo-500
+                      block
+                      w-full
+                      h-4/5
+                      shadow-sm
+                      sm:text-sm
+                      border-gray-300
+                      rounded-md
+                    "
+                  />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                  <label
+                    for="email_address"
+                    class="block text-sm font-medium text-gray-700"
+                    >Email address</label
+                  >
+                  <input
+                    type="email"
+                    class="
+                      mt-1
+                      focus:ring-indigo-500
+                      focus:border-indigo-500
+                      block
+                      w-full
+                      h-4/5
+                      shadow-sm
+                      sm:text-sm
+                      border-gray-300
+                      rounded-md
+                    "
+                  />
+                </div>
+
+                <div class="mt-1 col-span-6 sm:col-span-3">
+                  <label
+                    for="country"
+                    class="block text-sm font-medium text-gray-700"
+                    >Division</label
+                  >
+                  <select
+                    class="
+                      mt-1
+                      block
+                      w-full
+                      py-2
+                      px-3
+                      border border-gray-300
+                      bg-white
+                      rounded-md
+                      shadow-sm
+                      focus:outline-none
+                      focus:ring-indigo-500
+                      focus:border-indigo-500
+                      sm:text-sm
+                    "
+                  >
+                    <option>Sales & Marketing</option>
+                    <option>Operation</option>
+                    <option>IT</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                class="
+                  inline-flex
+                  justify-center
+                  py-2
+                  px-4
+                  border border-transparent
+                  shadow-sm
+                  text-sm
+                  font-medium
+                  rounded-md
+                  text-white
+                  bg-indigo-600
+                  hover:bg-indigo-700
+                  focus:outline-none
+                  focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                "
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    edit() {
+      console.log("ok");
+    },
+  },
+};
+</script>
